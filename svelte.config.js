bab7bef029c0f38ba7f432ce0b4f9aeed29efdeb
qyoh7 <speedrun7760@gmail.com>
2024-11-23 12:23:08 -0800

    steal code from svelte

--- svelte.config.js
+++ svelte.config.js
@@ -5,14 +5,11 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
 	kit: {
 		adapter: adapter({
-			// default options are shown. On some platforms
-			// these options are set automatically — see below
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-		})
+			fallback: '404.html'
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
 	}
 };
 
